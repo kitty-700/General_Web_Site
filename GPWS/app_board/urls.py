32e5058958f0b264d5b2dd359a9c2dd5bc97d13e
KITS <22@22.22>
2022-10-05 00:58:04 +0900

    (feat) 게시글 수정 기능 추가

--- GPWS/app_board/urls.py
+++ GPWS/app_board/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('write/', views.write_article, name='write_article'),
     path('<int:article_id>/', views.read_article, name='read_article'),
     path('<int:article_id>/write_comment', views.write_comment, name='write_comment'),
+    path('<int:article_id>/update_article', views.update_article, name='update_article'),
 
 ]
--- GPWS/app_board/views.py
+++ GPWS/app_board/views.py
@@ -16,11 +16,13 @@ def read_article(request:WSGIRequest, article_id:int):
         comments = get_list_or_404(Comment, article=article_id)
     except:
         comments = []
-    context = { 'article' : article, 'comments' : comments}
+    context = { 'article' : article, 'comments' : comments, 'ip': get_client_ip(request) }
     return render(request, 'app_board/read_article.html', context)
 
 def write_article(request:WSGIRequest):
     context = {}
+    ip = get_client_ip(request)
+
     if request.method == 'GET': # 1차로는 GET 으로 Form을 얻고
         write_form = ArticleWriteForm()
         context['forms'] = write_form
@@ -29,7 +31,7 @@ def write_article(request:WSGIRequest):
     elif request.method == 'POST': # 2차로는 받아온 Form에 내용을 넣어 입력 처리
         write_form = ArticleWriteForm(request.POST)
         if write_form.is_valid():
-            author = "Annonymous" # 추후 User.objects.get(user_id=login_session) 를 통해 User nickname 식별
+            author = ip # 추후 User.objects.get(user_id=login_session) 를 통해 User nickname 식별
             article = Article(
                 title=      write_form.title,
                 contents=  write_form.contents,
@@ -45,13 +47,46 @@ def write_article(request:WSGIRequest):
             return render(request, 'app_board/write_article.html', context)
 
 def write_comment(request:WSGIRequest, article_id:int):
-    print(request)
     if request.method == 'POST':
         comment = Comment(
             article_id=article_id,
             contents=request.POST['comment'],
         )
         comment.save()
+    return redirect('/app_board/%s/' % (article_id))
+
+def update_article(request:WSGIRequest, article_id:int):
+    context = {}
+    ip = get_client_ip(request)
+
     article = get_object_or_404(Article, pk=article_id)
 
-    return redirect('/app_board/%s/' % (article_id))
+    if article.author != ip:
+        return redirect('/app_board/')
+
+    if request.method == 'GET': # 1차로는 GET 으로 Form을 얻고
+        write_form = ArticleWriteForm(instance=article)
+        context['forms'] = write_form
+        return render(request, 'app_board/update_article.html', context)
+    elif request.method == 'POST': # 2차로는 받아온 Form에 내용을 넣어 입력 처리
+        write_form = ArticleWriteForm(request.POST)
+        if write_form.is_valid():
+            article.title = write_form.title
+            article.contents = write_form.contents
+            # article.modify_dt =
+            article.save()
+            return read_article(request=request, article_id=article_id)
+        else:
+            context['forms'] = write_form
+            if write_form.errors:
+                for val in write_form.errors.values():
+                    context['error'] = val
+            return render(request, 'app_board/write_article.html', context)
+
+def get_client_ip(request:WSGIRequest):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
