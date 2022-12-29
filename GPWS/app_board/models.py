9c7ca08cebac9343cd45cc402ec697ba20bb822e
KITS <22@22.22>
2022-12-29 23:40:35 +0900

    (uiux) 게시판 구분 문자열에 id 추가

--- GPWS/app_board/models.py
+++ GPWS/app_board/models.py
@@ -17,7 +17,7 @@ class Board(models.Model):
     title = models.CharField(max_length=300, null=False)
 
     def __str__(self):
-        return self.title
+        return f"{self.id} : {self.title}"
 
 class Article(models.Model):
     # PK 명시
